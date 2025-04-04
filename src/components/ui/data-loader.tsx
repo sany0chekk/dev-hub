9af2c16068d40af7237856c749a6110475023fb0
Oleksandr <olexandr.veseluy@gmail.com>
2025-04-04 13:52:04 +0300

    feat: update data-loader

--- src/components/ui/data-loader.tsx
+++ src/components/ui/data-loader.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export default function DataLoader({ children }: { children: ReactNode }) {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-2 items-center justify-center text-semibold text-lg">
+    <div className="mt-20 flex gap-2 items-center justify-center text-semibold text-lg">
       <Loader className="animate-spin" />
       {children}
     </div>
